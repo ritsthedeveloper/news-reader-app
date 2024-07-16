174de57f22d44a66412d7fea9309ddd0161f3165
Ritesh Gune <ritesh@tickledmedia.com>
2024-07-16 23:03:06 +0530

    Added test cases for saved articles

--- app/src/test/java/com/ritesh/newsreader/articles/data/repository/ArticleRepositoryTest.kt
+++ app/src/test/java/com/ritesh/newsreader/articles/data/repository/ArticleRepositoryTest.kt
@@ -4,11 +4,11 @@ import app.cash.turbine.test
 import com.ritesh.newsreader.AppConstants
 import com.ritesh.newsreader.articles.data.model.ApiArticle
 import com.ritesh.newsreader.articles.data.model.ArticlesResponse
+import com.ritesh.newsreader.articles.data.model.Country
 import com.ritesh.newsreader.articles.data.repository.database.DatabaseService
 import com.ritesh.newsreader.articles.data.repository.database.entity.Article
 import com.ritesh.newsreader.articles.data.repository.database.entity.Source
 import com.ritesh.newsreader.articles.data.repository.network.ApiInterface
-import com.ritesh.newsreader.common.ui.base.UIState
 import com.ritesh.newsreader.sources.data.model.ApiSource
 import com.ritesh.newsreader.sources.data.model.SourcesResponse
 import com.ritesh.newsreader.util.apiArticleListToArticleList
@@ -17,9 +17,9 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert
+import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
-import org.junit.Assert.assertEquals
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito
@@ -30,10 +30,13 @@ class ArticleRepositoryTest {
 
     // SUT
     private lateinit var articleRepository: ArticleRepository
+
     @Mock
     private lateinit var apiInterface: ApiInterface
+
     @Mock
     private lateinit var database: DatabaseService
+
     @Before
     fun setUp() {
         /**
@@ -46,7 +49,7 @@ class ArticleRepositoryTest {
     fun getNews_whenNetworkServiceResponseSuccess_shouldReturnSuccess() {
         runTest {
 
-            val response = getMockedSuccessResponse()
+            val response = getArticlesMockedSuccessResponse()
 
             Mockito.doReturn(response).`when`(apiInterface).getNews()
             Mockito.doReturn(
@@ -79,7 +82,7 @@ class ArticleRepositoryTest {
     @Test
     fun getNewsByLanguage_whenNetworkServiceResponseSuccess_shouldReturnSuccess() {
         runTest {
-            val response = getMockedSuccessResponse()
+            val response = getArticlesMockedSuccessResponse()
             Mockito.doReturn(response).`when`(apiInterface).getNewsByLang()
 
             articleRepository.getNewsByLanguage(AppConstants.DEFAULT_LANGUAGE).test {
@@ -92,7 +95,7 @@ class ArticleRepositoryTest {
     @Test
     fun getNewsBySource_whenNetworkServiceResponseSuccess_shouldReturnSuccess() {
         runTest {
-            val response = getMockedSuccessResponse()
+            val response = getArticlesMockedSuccessResponse()
             Mockito.doReturn(response).`when`(apiInterface).getNewsBySource()
 
             articleRepository.getNewsBySource(AppConstants.DEFAULT_SOURCE).test {
@@ -105,7 +108,7 @@ class ArticleRepositoryTest {
     @Test
     fun getNewsByCategory_whenNetworkServiceResponseSuccess_shouldReturnSuccess() {
         runTest {
-            val response = getMockedSuccessResponse()
+            val response = getArticlesMockedSuccessResponse()
             Mockito.doReturn(response).`when`(apiInterface).getNewsByCategory()
 
             articleRepository.getNewsByCategory(AppConstants.DEFAULT_CATEGORY).test {
@@ -118,7 +121,7 @@ class ArticleRepositoryTest {
     @Test
     fun searchNews_whenNetworkServiceResponseSuccess_shouldReturnSuccess() {
         runTest {
-            val response = getMockedSuccessResponse()
+            val response = getArticlesMockedSuccessResponse()
             Mockito.doReturn(response).`when`(apiInterface).searchNews("India world cup")
 
             articleRepository.searchNews("India world cup").test {
@@ -141,8 +144,46 @@ class ArticleRepositoryTest {
         }
     }
 
+    @Test
+    fun getSavedArticles_shouldReturnSuccess() {
+        runTest {
+            val response = getSavedArticlesMockedSuccessResponse()
+            Mockito.doReturn(flowOf(response)).`when`(database).getSavedArticles()
 
-    private fun getMockedSuccessResponse() : ArticlesResponse{
+            database.getSavedArticles().test {
+                assertEquals(response, awaitItem())
+                cancelAndIgnoreRemainingEvents()
+            }
+        }
+    }
+
+    private fun getSavedArticlesMockedSuccessResponse(): List<Article> {
+        // Success response mocked
+        val source = Source(id = "sourceId", name = "sourceName")
+        val article = Article(
+            id = 1,
+            source = source,
+            title = "title",
+            description = "description",
+            url = "url",
+            urlToImage = "urlToImage",
+            author = "author",
+            content = "content",
+            publishedAt = "pat"
+        )
+        val articles = mutableListOf<Article>()
+        articles.add(article)
+        return articles
+    }
+
+    private fun getCountriesMockedSuccessResponse(): List<Country> {
+        return listOf(
+            Country("India", "in"),
+            Country("Israel", "il"),
+        )
+    }
+
+    private fun getArticlesMockedSuccessResponse(): ArticlesResponse {
         // Success response mocked
         val source = Source(id = "sourceId", name = "sourceName")
         val article = ApiArticle(
@@ -162,7 +203,7 @@ class ArticleRepositoryTest {
         )
     }
 
-    private fun getSourcesMockedSuccessResponse() : SourcesResponse{
+    private fun getSourcesMockedSuccessResponse(): SourcesResponse {
         // Success response for Sources mocked
         val source = ApiSource(id = "sourceId", name = "sourceName")
         val sources = mutableListOf<ApiSource>()
